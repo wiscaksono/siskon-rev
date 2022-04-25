d5cd4fb35c4eaab782b1d9ca537cea47b808983c
wiscaksono <wwicaksono96@gmail.com>
2022-04-25 12:34:29 +0700

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <section className="wrapper">
+      <nav className="flex items-center justify-between">
+        <img src="logo.png" alt="" />
+        <div className="flex items-center gap-[32px] py-[20px]">
+          <Link href="/" passHref>
+            <button className="p-md text-[#230707]">Home</button>
+          </Link>
+          <Link href="/" passHref>
+            <button className="p-md text-[#230707]">Pilih Praktikum</button>
+          </Link>
+          <Link href="/" passHref>
+            <button className="p-md text-[#230707]">Gallery</button>
+          </Link>
+        </div>
+      </nav>
+    </section>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        sejarah: "url('/sejarah-bg.png')",
+        rektor: "url('/rektor-bg.png')",
+        team: "url('/team-bg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
